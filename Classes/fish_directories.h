1ab85175a022cda20b3372df9719719d2bc1bd18
Hugh Krogh-Freeman <hughkf@gmail.com>
2015-10-02 11:08:41 -0700

    intermediate commit #1

---
+++ Classes/fish_directories.h
@@ -0,0 +1,22 @@
+//
+//  fish_directories.h
+//  MobileTerminal
+//
+//  Created by Hugh Krogh-Freeman on 10/2/15.
+//
+//
+
+#ifndef fish_directories_h
+#define fish_directories_h
+
+//#define DOCDIR "fish/docs"
+//#define DATADIR "fish/data"
+//#define SYSCONFDIR "fish/sysconfig"
+//#define BINDIR "fish/bind"
+
+#define DOCDIR "."
+#define DATADIR "."
+#define SYSCONFDIR "."
+#define BINDIR "."
+
+#endif /* fish_directories_h */
